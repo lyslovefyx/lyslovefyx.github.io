4e74e9ed89d9aadec8eb72ffff892642fb319cdd
lyslovefyx <597217818@qq.com>
2020-05-04 18:32:43 +0800

    10个G歌曲上传完毕

--- index_files/functions.js
+++ index_files/functions.js
@@ -38,8 +38,8 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var current = new Date();
+	var seconds = (Date.parse(current) - Date.parse("2017/12/30")) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
